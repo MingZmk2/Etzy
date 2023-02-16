f26cc1aa06f66eee2a7e81104db0eccceabe58b0
Ming Zhang <mzhang630@gmail.com>
2023-02-15 23:56:31 -0500

    fixed footer styling again

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -4,73 +4,80 @@ import "./index.css";
 
 const Footer = () => {
   return (
-    <div className="footer-container">
-      <div className="footer-col">
-        <h1>Projects</h1>
-        <div className="footer-list-container">
-          <a
-            href="https://mingzmk2.github.io/Operation-Helix/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Operation Helix
-          </a>
-          <a
-            href="https://rebindr.onrender.com/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            reBindr
-          </a>
-          {/* <a href="https://www.heyitsming.com/" target="_blank">
-          Portfolio
-        </a> */}
-        </div>
-      </div>
-      <div className="footer-col">
-        <h1>Language</h1>
-        <div className="footer-list-container">
-          <p>JavaScript - Frontend</p>
-          <p>Ruby - Backend</p>
-        </div>
-      </div>
-      <div className="footer-col">
-        <h1>Technology</h1>
-        <div className="footer-list-container">
-          <p>React & Redux</p>
-          <p>HTML5/CSS3</p>
-          <p>Ruby on Rails</p>
-          <p>AWS S3</p>
-          <p>PostgreSQL</p>
-        </div>
-      </div>
-      <div className="footer-col">
-        <div className="help-container">
-          <h1>Social</h1>
-        </div>
+    <div className="footer-outer-container">
+      <div className="footer-inner-container">
+        <div className="footer-col">
+          <h1>Projects</h1>
+          <ul className="footer-list-container">
+            <li>
+              <a
+                href="https://mingzmk2.github.io/Operation-Helix/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Operation Helix
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://rebindr.onrender.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                reBindr
+              </a>
+            </li>
 
-        <div className="social-links">
-          <a
-            href="https://github.com/MingZmk2"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src="../github.png" alt="Github" />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/mingzhang8/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src="../linkedin.png" alt="LinkedIn" />
-          </a>
-          <a
-            href="https://angel.co/u/ming-zhang-18"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src="../angellist.png" alt="AngelList" />
-          </a>
+            {/* <a href="https://www.heyitsming.com/" target="_blank">
+            My Portfolio
+          </a> */}
+          </ul>
+        </div>
+        <div className="footer-col">
+          <h1>Language</h1>
+          <div className="footer-list-container">
+            <p>JavaScript - Frontend</p>
+            <p>Ruby - Backend</p>
+          </div>
+        </div>
+        <div className="footer-col">
+          <h1>Technology</h1>
+          <div className="footer-list-container">
+            <p>React & Redux</p>
+            <p>HTML5/CSS3</p>
+            <p>Ruby on Rails</p>
+            <p>AWS S3</p>
+            <p>PostgreSQL</p>
+          </div>
+        </div>
+        <div className="footer-col">
+          <div className="help-container">
+            <h1>Social</h1>
+          </div>
+
+          <div className="social-links">
+            <a
+              href="https://github.com/MingZmk2"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src="../github.png" alt="Github" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/mingzhang8/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src="../linkedin.png" alt="LinkedIn" />
+            </a>
+            <a
+              href="https://angel.co/u/ming-zhang-18"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src="../angellist.png" alt="AngelList" />
+            </a>
+          </div>
         </div>
       </div>
     </div>
